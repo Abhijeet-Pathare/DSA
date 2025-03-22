ae2044d3ff75462233ff3fad5a454e6ce75adebd
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-03-22 10:43:42 +0530

    prepend

--- Big O/Main.java
+++ Big O/Main.java
@@ -3,9 +3,11 @@ public class Main {
         LinkedList myList = new LinkedList(4);
         //myList.printList();
         myList.append(5);
-        myList.printList();
+//        myList.printList();
         myList.append(6);
-        System.out.println("removing 6");
-        System.out.println(myList.removeLast().value);
+//        System.out.println("removing 6");
+//        System.out.println(myList.removeLast().value);
+        myList.prepend(3);
+        myList.printList();
     }
 }
