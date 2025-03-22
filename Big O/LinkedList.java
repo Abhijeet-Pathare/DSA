d551af2dd741e4fe58e61d60fd677843368c1ef2
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-03-22 10:10:15 +0530

    linked list append, print methods

--- Big O/LinkedList.java
+++ Big O/LinkedList.java
@@ -3,29 +3,51 @@ public class LinkedList {
     Node tail;
     int length;
 
-    class Node{
+    class Node {
         int value;
         Node next;
 
-        public Node(int value){
+        public Node(int value) {
             this.value = value;
         }
     }
 
-    public LinkedList(int value){
+    public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    Node getHead(){
+    Node getHead() {
         return head;
     }
-    Node getTail(){
+
+    Node getTail() {
         return tail;
     }
-    int getLength(){
+
+    int getLength() {
         return length;
     }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+        public void append ( int value){
+            Node newNode = new Node(value);
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+            length++;
+        }
 }
---
+++ Big O/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myList = new LinkedList(4);
+        //myList.printList();
+        myList.append(5);
+        myList.printList();
+        myList.append(6);
+        myList.printList();
+    }
+}
