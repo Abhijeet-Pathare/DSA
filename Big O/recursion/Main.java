9b27665218579fb324660fd492b6f4a6bd80a890
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-10-01 17:36:16 +0530

    recursion code

---
+++ Big O/recursion/Main.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class Main {
+
+    public static void method3(){
+        System.out.println("Three");
+    }
+    public static void method2(){
+        method3();
+        System.out.println("Two");
+    }
+    public static void method1(){
+        method2();
+        System.out.println("One");
+    }
+
+    public static void main(String[] args) {
+        method1();
+    }
+}
